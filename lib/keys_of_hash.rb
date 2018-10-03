944783ac390fccb57afde89ce1c8b987aaf66b4e
Bryce Eadie <bryce.eadie@gmail.com>
2018-10-03 03:27:48 +0000

    automatically backed up by learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,11 +1,27 @@
+
 class Hash
-  def keys_of_hash(*arguments)
-    results = []
-    arguments.each do |key, value|
-      if arguments.include? (value)
-        results.push(key)
+  def keys_of(x, *arguments)
+    # code goes here
+    result = []
+    
+    if arguments.length == 0
+    
+      self.each do|k,v| 
+        if v == x
+          result << k
+        end
+      end
+    else 
+      arguments.each do |arg|
+        self.each do |k,v|
+          if v == x || v == arg
+            result << k
+          end
+        end
       end
     end
-    results
+
+    result
+
   end
 end

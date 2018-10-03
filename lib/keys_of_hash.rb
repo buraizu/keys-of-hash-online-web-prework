80155af35f2dab25ff059e889eef9881f575988d
Bryce Eadie <bryce.eadie@gmail.com>
2018-10-03 03:42:32 +0000

    automatically backed up by learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -2,6 +2,12 @@
 class Hash
   def keys_of(value, *hashes)
    array = []
-    
+      if hashes.include?(value)
+        hashes.each do |key, value2|
+          array << value2
+        end
+      end 
+    end
+    return array
   end
 end

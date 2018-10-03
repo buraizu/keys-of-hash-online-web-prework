c8ecada5c5a0f146f35d98928bb61e01416a7dfe
Bryce Eadie <bryce.eadie@gmail.com>
2018-10-03 02:57:02 +0000

    automatically backed up by learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -3,13 +3,5 @@ require 'pry'
 class Hash
   def keys_of_hash(hash, *value)
     array = []
-    puts hash.keys
-    
-     binding.pry
-    value.each do
-     puts value
-    
-    end
-    return array
-  end
+   
 end

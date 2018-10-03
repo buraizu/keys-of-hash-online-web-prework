665ecf83e7054a1efc218bb97ec62a412db41d8e
Bryce Eadie <bryce.eadie@gmail.com>
2018-10-03 04:43:42 +0000

    automatically backed up by learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -3,13 +3,11 @@
 class Hash
   def keys_of(value, *hashes)
     result_array = []
-      hashes.each do |hash|
-        self.each do |key, val|
-          if val == value
-            result_array << val
-          end
+      hashes.each do |key, val|
+        if val == value
+          result_array << val
         end
       end
+    end
     return result_array
   end
-end

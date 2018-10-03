d784d1de378a610d7fb41b77875290b013851c00
Bryce Eadie <bryce.eadie@gmail.com>
2018-10-03 03:25:07 +0000

    automatically backed up by learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,7 +1,7 @@
 class Hash
   def keys_of_hash(*arguments)
     results = []
-    self.each do |key, value|
+    arguments.each do |key, value|
       if arguments.include? (value)
         results.push(key)
       end
